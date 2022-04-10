a9a1363fb13a1760775daf1e39f30b5f775177e2
BrOleg5 <oleg.brylyov@mail.ru>
2022-04-10 22:07:39 +0700

    Add scripts folder with backup control scripts.

---
+++ scripts/BackupControl.ps1
@@ -0,0 +1,39 @@
+#requires -version 5.1
+
+Import-Module -FullyQualifiedName "E:\Projects\Scripts\AutoBackupScripts\modules\BackupModule.psm1"
+Import-Module -FullyQualifiedName "E:\Projects\Scripts\AutoBackupScripts\modules\AndroidBackup.psm1"
+
+$BackupDestination = "F:\Backup"
+
+$PCSources =    "E:\AutoHotKey Scripts", `
+                "E:\Books", `
+                "E:\Conferences", `
+                "E:\Documents", `
+                "E:\Events", `
+                "E:\Games", `
+                "E:\KeePass", `
+                "E:\Music", `
+                "E:\Papers", `
+                "E:\Photo and Video", `
+                "E:\Projects", `
+                "E:\Study", `
+                "E:\Templates and profiles", `
+                "E:\Wallpapers and avatars", `
+                "E:\Work",
+                "E:\Other",
+                "C:\Users\Oleg\.config\joplin-desktop"
+
+Backup-Process -Sources $PCSources -Destination $BackupDestination
+
+$AndroidSources =  "phone/DCIM",
+            "sdcard/DCIM/Camera",
+            "sdcard/Music"
+
+# For run adb daemon in smartphone
+Write-Host 'Connect you smartphone to computer. Press any key to continue...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+adb devices
+
+Start-Sleep -Seconds 20
+
+Backup-Android -Sources $AndroidSources -Destination "$BackupDestination\Android"
