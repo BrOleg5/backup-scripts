5f5e1925f6e03c0f3a46e5a03f9286315bc2b000
BrOleg5 <oleg.brylyov@mail.ru>
2023-01-16 00:07:12 +0800

    Add multiple backup destination. Add class to ordered store source and destination paths.

--- scripts/BackupControl.ps1
+++ scripts/BackupControl.ps1
@@ -5,79 +5,77 @@ $ModulePath = "E:\Projects\Scripts\AutoBackupScripts\modules"
 Import-Module -FullyQualifiedName "$ModulePath\BackupModule.psm1"
 Import-Module -FullyQualifiedName "$ModulePath\AndroidBackup.psm1"
 
-$BackupDestination = "F:\Backup"
+class SourceDestination {
+    [string[]] $Source
+    [string[]] $Destination
 
-$PCSources = "E:\AutoHotKey Scripts",
-             "E:\Books",
-             "E:\Conferences",
-             "E:\Documents",
-             "E:\Events",
-             "E:\Games",
-             "E:\KeePass",
-             "E:\Music",
-             "E:\Photo and Video",
-             "E:\Projects",
-             "E:\Study",
-             "E:\Templates and profiles",
-             "E:\Wallpapers and avatars",
-             "E:\Work",
-             "E:\Other",
-             "C:\Users\Oleg\.config\joplin-desktop",
-             "C:\Users\Oleg\AppData\Roaming\Thunderbird\Profiles",
-             "C:\Users\Oleg\Documents\KiCad",
-             "C:\Users\Oleg\AppData\Roaming\Code\User\hsnips",
-             "C:\Users\Oleg\AppData\Roaming\Code\User\keybindings.json",
-             "C:\Users\Oleg\AppData\Roaming\Code\User\settings.json",
-             "C:\Users\Oleg\texmf\tex\latex"
+    [void] AddItem([string] $src, [string] $dst) {
+        $this.Source += $src
+        $this.Destination += $dst
+    }
+}
 
-$PCDestination = "$BackupDestination\AutoHotKey Scripts",
-                 "$BackupDestination\Books",
-                 "$BackupDestination\Conferences",
-                 "$BackupDestination\Documents",
-                 "$BackupDestination\Events",
-                 "$BackupDestination\Games",
-                 "$BackupDestination\KeePass",
-                 "$BackupDestination\Music",
-                 "$BackupDestination\Photo and Video",
-                 "$BackupDestination\Projects",
-                 "$BackupDestination\Study",
-                 "$BackupDestination\Templates and profiles",
-                 "$BackupDestination\Wallpapers and avatars",
-                 "$BackupDestination\Work",
-                 "$BackupDestination\Other",
-                 "$BackupDestination\Users\Oleg\.config\joplin-desktop",
-                 "$BackupDestination\Users\Oleg\AppData\Roaming\Thunderbird\Profiles",
-                 "$BackupDestination\Users\Oleg\Documents\KiCad",
-                 "$BackupDestination\Users\Oleg\AppData\Roaming\Code\User\hsnips",
-                 "$BackupDestination\Users\Oleg\AppData\Roaming\Code\User",
-                 "$BackupDestination\Users\Oleg\AppData\Roaming\Code\User",
-                 "$BackupDestination\Users\Oleg\texmf\tex\latex"
-                
-Backup-Process -Sources $PCSources -Destinations $PCDestination -PathToLogFile "$BackupDestination\.log\log pc.txt"
+$PCSourceDestination = [SourceDestination]::new()
+
+$PCSourceDestination.AddItem("E:\AutoHotKey Scripts",                                       "AutoHotKey Scripts")
+$PCSourceDestination.AddItem("E:\Books",                                                    "Books")
+$PCSourceDestination.AddItem("E:\Documents",                                                "Documents")
+$PCSourceDestination.AddItem("E:\Events",                                                   "Events")
+$PCSourceDestination.AddItem("E:\Games",                                                    "Games")
+$PCSourceDestination.AddItem("E:\KeePass",                                                  "KeePass")
+$PCSourceDestination.AddItem("E:\Music",                                                    "Music")
+$PCSourceDestination.AddItem("E:\Photo and Video",                                          "Photo and Video")
+$PCSourceDestination.AddItem("E:\Projects",                                                 "Projects")
+$PCSourceDestination.AddItem("E:\Study",                                                    "Study")
+$PCSourceDestination.AddItem("E:\Templates and profiles",                                   "Templates and profiles")
+$PCSourceDestination.AddItem("E:\Wallpapers and avatars",                                   "Wallpapers and avatars")
+$PCSourceDestination.AddItem("E:\Work",                                                     "Work")
+$PCSourceDestination.AddItem("E:\Other",                                                    "Other")
+$PCSourceDestination.AddItem("C:\Users\Oleg\.config\joplin-desktop",                        "Users\Oleg\.config\joplin-desktop")
+$PCSourceDestination.AddItem("C:\Users\Oleg\AppData\Roaming\Thunderbird\Profiles",          "Users\Oleg\AppData\Roaming\Thunderbird\Profiles")
+$PCSourceDestination.AddItem("C:\Users\Oleg\Documents\KiCad",                               "Users\Oleg\Documents\KiCad")
+$PCSourceDestination.AddItem("C:\Users\Oleg\AppData\Roaming\Code\User\hsnips",              "Users\Oleg\AppData\Roaming\Code\User\hsnips")
+$PCSourceDestination.AddItem("C:\Users\Oleg\AppData\Roaming\Code\User\keybindings.json",    "Users\Oleg\AppData\Roaming\Code\User")
+$PCSourceDestination.AddItem("C:\Users\Oleg\AppData\Roaming\Code\User\settings.json",       "Users\Oleg\AppData\Roaming\Code\User")
+$PCSourceDestination.AddItem("C:\Users\Oleg\texmf\tex\latex",                               "Users\Oleg\texmf\tex\latex")
+
+$BackupDestinations = "F:\Backup", "H:\Backup"
+
+foreach ($destination in $BackupDestinations) {
+    $FullDestinations = $PCSourceDestination.Destination | ForEach-Object -Process {Join-Path -Path $destination -ChildPath $_}
+    
+    Backup-Process -Sources $PCSourceDestination.Source -Destinations $FullDestinations -PathToLogFile "$destination\.log\log pc.txt"
+}
 
 # Android serial number
+# To find out serial numbe
+#  1) connect your phone to computer by USB
+#  2) turn on USB Debigging (in Developer options)
+#  3) run command `adb devices` in shell
+# You see serial number of your phone in command output
 $SerialNumber = "80fbca55"
 # Internal Andriod storage
 $Phone = "sdcard"
 # Android SD-card
 $SDCard = "storage/5C11-6780"
 
-$AndroidSources =  "$Phone/DCIM",
-                   "$SDCard/DCIM/Camera",
-                   "$SDCard/Music"
-                    
-$AndroidDestination = "$BackupDestination\Android"
-$ADestinations = "$AndroidDestination\Phone",
-                 "$AndroidDestination\SD card",
-                 "$AndroidDestination\SD card"
+$AndroidSourceDestination = [SourceDestination]::new()
+
+$AndroidSourceDestination.AddItem("$Phone/DCIM", "Android\Phone")
+$AndroidSourceDestination.AddItem("$SDCard/DCIM/Camera", "Android\SD card")
+$AndroidSourceDestination.AddItem("$SDCard/Music", "Android\SD card")
 
 # Wait connect device
 Write-Host "Wait to connect smartphone (serial number: $SerialNumber) to computer ..."
 adb -s $SerialNumber wait-for-device
 Write-Host "Connection successful."
 
-Backup-Android -SerialNumber $SerialNumber -Sources $AndroidSources -Destinations $ADestinations `
-               -PathToLogFile "$BackupDestination\.log\log android.txt"
+foreach ($destination in $BackupDestinations) {
+    $FullDestinations = $AndroidSourceDestination.Destination | ForEach-Object -Process {Join-Path -Path $destination -ChildPath $_}
+    
+    Backup-Android -SerialNumber $SerialNumber -Sources $AndroidSourceDestination.Source -Destinations $FullDestinations `
+               -PathToLogFile "$destination\.log\log android.txt"
+}
 
 Write-Host "Backup Android device is successful. Stop adb server."
 adb kill-server
